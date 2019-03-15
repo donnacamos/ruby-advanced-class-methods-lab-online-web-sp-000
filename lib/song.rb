ec44c00bd763b345a7ab7585bed3b95fe1a535da
Donna Amos <donnacamos.developer@gmail.com>
2019-03-15 02:12:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,7 +52,7 @@ end
   song_array = filename.split(" - ")
   song_array[1] = song_array[1].chomp(".mp3")
   song = self.new 
-  
+  song.name = 
   end 
   
   def self.create_from_filename(filename)
