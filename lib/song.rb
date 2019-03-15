fe7acd94ea0fab4da8c42115d7a5fc77a46dd3b3
Donna Amos <donnacamos.developer@gmail.com>
2019-03-15 02:09:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,6 +57,11 @@ end
   end 
   
   def self.create_from_filename(filename)
-    
+     result = self.new_from_filename(filename)
+    song = self.create 
+    song.name = result.name 
+    song.artist_name = result.artist_name 
+    song 
+  end 
   
 end 
